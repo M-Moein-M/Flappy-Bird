9dffc83324f75a0c3875cf70f79a8ef95b207f18
mohammadmoein.moradi@gmail.com <mohamamdmoein.moradi@gamil.com>
2020-08-18 15:03:25 +0430

    Now all the collisions will be detected and the game will stop after collision

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ function gameInit() {
     let gravityAcceleration;
     let currentBirdVelocityY;
     let gameFrameDiv = document.getElementById('game-frame');
-
     let currentObstacleID = 0; // after creating new obstacle we set the id equal to this number and increase the value by 1
     // 0top for the top obstacle, 0bottom for the bottom obstacle
+
+    const obstacleWidth = 8;
 
     let createObstacleInterval;
     let moveObstacleInterval;
@@ -31,7 +32,47 @@ function gameInit() {
         }
     }
 
-    function removeOutOFGameFrameObstacle(obstacle){  // removes the obstacle if it reaches the left side of the gameFrame
+    function checkForCollision() {
+        let birdImage = document.getElementById('bird-img');
+        const offset = 50;  // indicates how much we can move out side off the game frame
+        if (getElementPosition(birdImage, 'bottom') > gameFrameDiv.offsetHeight + offset ||
+            getElementPosition(birdImage, 'bottom') + birdImage.offsetHeight < -offset / 2)
+            clearAllIntervals();  // means the game will freeze
+
+        let allObstacles = gameFrameDiv.children;
+        for (let obs of allObstacles) {
+            if (obs.classList.contains('obstacle')) {
+                const obstacleLeft = getElementPosition(obs, 'left');
+                const obstacleRight = obstacleLeft + obstacleWidth;
+                const birdLeft = getElementPosition(birdImage, 'left');
+                const birdRight = birdLeft + birdImage.offsetWidth;
+                if (birdLeft < obstacleRight && birdRight > obstacleLeft) { // there's a chance that bird hits the obstacle ( checking for in range x)
+                    let obstacleTop;
+                    let obstacleBottom;
+                    if (obs.id.includes('top')) { // if this is a top obstacle it means that it only has style.top
+                        obstacleTop = gameFrameDiv.offsetHeight;
+                        obstacleBottom = obstacleTop - document.getElementById(obs.id).offsetHeight;
+                    } else if (obs.id.includes('bottom')) {  // if this is a bottom obstacle it means that it only has style.bottom
+                        obstacleBottom = 0;
+                        obstacleTop = obstacleBottom + document.getElementById(obs.id).offsetHeight;
+                    }
+                    const birdBottom = getElementPosition(birdImage, 'bottom');
+                    const birdTop = birdBottom + birdImage.offsetHeight;
+                    if (birdTop > obstacleBottom && birdBottom < obstacleTop)
+                        clearAllIntervals();
+                }
+            }
+        }
+
+    }
+
+    function clearAllIntervals() {
+        clearInterval(moveObstacleInterval);
+        clearInterval(createObstacleInterval);
+        clearInterval(movementInterval);
+    }
+
+    function removeOutOFGameFrameObstacle(obstacle) {  // removes the obstacle if it reaches the left side of the gameFrame
         if (getElementPosition(obstacle, 'left') < 0)
             obstacle.remove();
     }
@@ -62,7 +103,6 @@ function gameInit() {
         gameFrameDiv.appendChild(newObstacleBottom);
 
         function setInitialAttributes(element) {
-            const obstacleWidth = 8;
 
             element.classList.add('obstacle');
             element.style.width = obstacleWidth + 'px';  // set the width of obstacle
@@ -86,14 +126,18 @@ function gameInit() {
         let movementDelay = 20;
 
         let birdImage = document.getElementById('bird-img');
-        birdImage.style.bottom = (Math.floor(2 * gameFrameDiv.offsetHeight / 3)).toString() + 'px';
-        console.log('bottom:' + getElementPosition(birdImage, 'bottom'));
+
+        setElementPosition(birdImage, 'bottom', (Math.floor(2 * gameFrameDiv.offsetHeight / 3)))
+        setElementPosition(birdImage, 'left', 30);
+
 
         movementInterval = setInterval(function () {
             let y = getElementPosition(birdImage, 'bottom') + currentBirdVelocityY;
             currentBirdVelocityY += gravityAcceleration;
             setElementPosition(birdImage, 'bottom', y);
+            checkForCollision();
         }, movementDelay);
+
 
     }
 
